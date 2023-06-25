052608784721f4d830601a872c0ba0ce7c063f0e
pavelminsk1979 <pavelminsk1979@mail.ru>
2023-06-25 08:49:49 +0300

    refactor....

--- src/f3-differernt/MessageOnMail.tsx
+++ src/f3-differernt/MessageOnMail.tsx
@@ -59,6 +59,7 @@ export const MessageOnMail = () => {
                                            {...formik.getFieldProps('message')}/>
                             </label>
                             <button  type='submit'>ОТПРАВИТЬ</button>
+                            <div>KJJHL:KJLK:JLKJ:LKJKL::LKJL:KJL:KJ:JLKJLK</div>
                             <button  >ТРЕНИРОВАЧНАЯ</button>
                         </form>
                 </div>
